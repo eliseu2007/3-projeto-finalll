2e26924327e3f440e030c5cd794d0c4acd919d91
eliseu2007 <00001103935744sp@al.educacao.sp.gov.br>
2024-11-13 08:55:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'literatura',
+    'Quem escreveu o livro O Principe?',
+    'Maquiavel'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'Brasilia'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 1 + 1 ?',
+    '2'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz amor em inglês?',
+    'Love em ingles é love (love)'
+)
